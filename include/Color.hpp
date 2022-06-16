240a5e5c4525f5522e9254bff8e7fefb24dc8ebc
srakuma <robben2159@gmail.com>
2022-06-16 22:50:30 +0900

    enable formatter Color*

---
+++ include/Color.hpp
@@ -0,0 +1,26 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+class Color {
+  private:
+	int _trgb;
+
+  public:
+	Color(void);
+	Color(unsigned char t, unsigned char r, unsigned char g, unsigned char b);
+	Color(const Color& another);
+	~Color(void);
+	Color& operator=(const Color& another);
+	unsigned char getTransparency(void);
+	unsigned char getRed(void);
+	unsigned char getGreen(void);
+	unsigned char getBlue(void);
+	void setTransparency(unsigned char t);
+	void setRed(unsigned char r);
+	void setGreen(unsigned char g);
+	void setBlue(unsigned char b);
+	int getOpposite(void);
+	void addShade(double distance);
+};
+
+#endif /* COLOR_HPP */

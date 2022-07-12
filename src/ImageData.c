35550cac8dad7badc404ec51dd863efec975a041
srakuma <robben2159@gmail.com>
2022-07-12 19:21:11 +0900

    add circle func

--- src/ImageData.c
+++ src/ImageData.c
@@ -1,15 +1,13 @@
-#include <mlx.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "ImageData.h"
+#include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-t_image_data	*init_image_data(void *mlx, int height, int width)
-{
-	t_image_data	*new;
+t_image_data* init_image_data(void* mlx, int height, int width) {
+	t_image_data* new;
 
-	new = (t_image_data *)malloc(sizeof(t_image_data));
-	if (new == NULL)
-	{
+	new = (t_image_data*)malloc(sizeof(t_image_data));
+	if (new == NULL) {
 		printf("failed to initialize image data\n");
 		exit(1);
 	}
@@ -18,29 +16,27 @@ t_image_data	*init_image_data(void *mlx, int height, int width)
 		printf("faild to make new image\n");
 		exit(1);
 	}
-	new->_addr = mlx_get_data_addr(new->_img, &(new->_bits_per_pixel), \
-										&(new->_line_length), &(new->_endian));
+	new->_addr = mlx_get_data_addr(new->_img, &(new->_bits_per_pixel),
+	                               &(new->_line_length), &(new->_endian));
 	return (new);
 }
 
-static void	my_mlx_pixel_put(t_image_data *data, int x, int y, int color)
-{
-	char	*dst;
+static void my_mlx_pixel_put(t_image_data* data, int x, int y, int color) {
+	char* dst;
 
-	dst = data->_addr + (y * data->_line_length + x * (data->_bits_per_pixel / 8));
+	dst = data->_addr +
+	      (y * data->_line_length + x * (data->_bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
-static int	absolute(int x)
-{
+static int absolute(int x) {
 	if (x < 0)
 		return (-x);
 	return (x);
 }
 
-static int	init_line_data(t_coordinate *start, t_coordinate *end, \
-							t_coordinate *delta, t_coordinate *steep)
-{
+static int init_line_data(t_coordinate* start, t_coordinate* end,
+                          t_coordinate* delta, t_coordinate* steep) {
 	if (!start || !end)
 		return (1);
 	delta->x = absolute(end->x - start->x);
@@ -58,36 +54,40 @@ static int	init_line_data(t_coordinate *start, t_coordinate *end, \
 
 /**
  * @brief Bresenham's line algorithm, it is draw line from start to end
- * if return value more than 1 it is failed to draw line for some reason, othrewise it is ok
+ * if return value more than 1 it is failed to draw line for some reason,
+ * othrewise it is ok
  *
  * @param start
  * @param end
  * @param data data of image
  * @param color
  */
-int	draw_line(t_coordinate* start, t_coordinate *end, \
-					t_image_data *data, t_color color)
-{
-	t_coordinate	delta;
-	t_coordinate	tmp;
-	t_coordinate	steep;
-	int				error;
+int draw_line(t_coordinate* start, t_coordinate* end, t_image_data* data,
+              t_color color) {
+	t_coordinate delta;
+	t_coordinate tmp;
+	t_coordinate steep;
+	int error;
 
 	if (init_line_data(start, end, &delta, &steep))
 		return (1);
-	tmp = (t_coordinate){start->x, start->y};
+	tmp   = (t_coordinate){start->x, start->y};
 	error = delta.x - delta.y;
-	while (1)
-	{
+	while (1) {
 		my_mlx_pixel_put(data, tmp.x, tmp.y, color);
 		if (tmp.x == end->x && tmp.y == end->y)
 			break;
-		if ((2 * error) > -delta.y) // Whether adding delta.y to the current x coordinate does not exceed half of the x increase
+		if ((2 * error) >
+		    -delta.y) // Whether adding delta.y to the current x coordinate does
+		              // not exceed half of the x increase
 		{
 			error -= delta.y;
 			tmp.x += steep.x;
 		}
-		else // If it is exceeded, add 1 to the y-coordinate and add x to error again to take into account the slope.
+		if ((2 * error) <
+		    delta
+		        .x) // If it is exceeded, add 1 to the y-coordinate and add x to
+		//  error again to take into account the slope.
 		{
 			error += delta.x;
 			tmp.y += steep.y;
@@ -96,55 +96,89 @@ int	draw_line(t_coordinate* start, t_coordinate *end, \
 	return (0);
 }
 
-int	draw_circle(t_coordinate *center, unsigned int radius, \
-					t_image_data *data, t_color color)
-{
-	t_coordinate	start;
-	t_coordinate	tmp;
-	int				error;
-	int				x_first;
-
+int draw_circle(t_coordinate* center, unsigned int radius, t_image_data* data,
+                t_color color) {
 	if (center == NULL)
 		return (1);
-	if (center->x < radius || center->y < radius
-		|| WIDTH < (center->x + radius) || HEIGHT < (center->y + radius))
+	if (center->x < radius || center->y < radius ||
+	    WIDTH < (center->x + radius) || HEIGHT < (center->y + radius))
 		return (2);
-	start = (t_coordinate){center->x - radius, center->y};
-	//tmp = start;
-	tmp = (t_coordinate){start.x, start.y};
-	x_first = 1;
-	error = 5;
-	while (2 * tmp.y != (2 * start.y) + radius)
-	{
-		my_mlx_pixel_put(data, tmp.x, tmp.y, color);
-		if (error > 0)
-		{
-			x_first += 2;
-			error += (x_first * tmp.x - center->x + 2) << 2;
-			tmp.x++;
-		}
-		tmp.y++;
-		error += /*diff.y*/ ((tmp.y - center->y) + 1) << 3;
-		printf("error : %d\n center->x: %d, tmp.x: %d\n center->y: %d, tmp.y: %d\n", \
-				error, center->x, tmp.x, center->y, tmp.y);
+
+	t_coordinate now = (t_coordinate){radius, 0};
+	while (absolute(now.x) >= absolute(now.y)) {
+		my_mlx_pixel_put(data, center->x + now.x, center->y + now.y, color);
+		my_mlx_pixel_put(data, center->x - now.x, center->y + now.y, color);
+		my_mlx_pixel_put(data, center->x + now.x, center->y - now.y, color);
+		my_mlx_pixel_put(data, center->x - now.x, center->y - now.y, color);
+		my_mlx_pixel_put(data, center->x + now.y, center->y + now.x, color);
+		my_mlx_pixel_put(data, center->x - now.y, center->y + now.x, color);
+		my_mlx_pixel_put(data, center->x + now.y, center->y - now.x, color);
+		my_mlx_pixel_put(data, center->x - now.y, center->y - now.x, color);
+		// 下
+		long double dist1 =
+		    (now.x) * (now.x) + (now.y + 1) * (now.y + 1) - radius * radius;
+		// 左下
+		long double dist2 = (now.x - 1) * (now.x - 1) +
+		                    (now.y + 1) * (now.y + 1) - radius * radius;
+		now.y++;
+		if (dist1 > dist2)
+			now.x--;
 	}
-	return (0);
+	return 0;
 }
 
+// int draw_circle(t_coordinate *center, unsigned int radius, t_image_data
+// *data, 				t_color color)
+// {
+// 	t_coordinate start;
+// 	t_coordinate tmp;
+// 	int error;
+// 	int x_coefficient;
+
+// 	if (center == NULL)
+// 		return (1);
+// 	if (center->x < radius || center->y < radius ||
+// 		WIDTH < (center->x + radius) || HEIGHT < (center->y + radius))
+// 		return (2);
+// 	// start = (t_coordinate){center->x - radius, center->y};
+// 	start = (t_coordinate){center->x + radius, center->y};
+// 	//   tmp = start;
+// 	tmp = (t_coordinate){start.x, start.y};
+// 	x_coefficient = 1;
+// 	int y_coff = 1;
+// 	error = 5;
+// 	while (2 * tmp.y != (2 * start.y) + radius)
+// 	{
+// 		my_mlx_pixel_put(data, tmp.x, tmp.y, color);
+// 		if (error > 0)
+// 		{
+// 			x_coefficient += 2;
+// 			// error += (x_coefficient * tmp.x - center->x + 2) << 2;
+// 			error -= (x_coefficient * tmp.x - center->x) << 2;
+// 			error += 8;
+// 			//  tmp.x++;
+// 			tmp.x--;
+// 		}
+// 		tmp.y++;
+// 		y_coff += 2;
+// 		error += ((tmp.y - center->y) + y_coff) << 3;
+// 		printf("error : %d\n center->x: %d, tmp.x: %d\n center->y: %d, tmp.y: "
+// 			   "%d\n",
+// 			   error, center->x, tmp.x, center->y, tmp.y);
+// 	}
+// 	return (0);
+// }
+
 // create_triangleで実装したい
-void	create_square(t_image_data *data, int height, int width, int color)
-{
-	for (size_t i = 0; i < height; i++)
-	{
-		for (size_t j = 0; j < width; j++)
-		{
+void create_square(t_image_data* data, int height, int width, int color) {
+	for (size_t i = 0; i < height; i++) {
+		for (size_t j = 0; j < width; j++) {
 			my_mlx_pixel_put(data, i, j, color);
 		}
 	}
 }
 
-void	destruct_image_data(void *mlx, t_image_data *data)
-{
+void destruct_image_data(void* mlx, t_image_data* data) {
 	mlx_destroy_image(mlx, data->_img);
 	free(data);
 }
--- src/test.c
+++ src/test.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-#include <math.h>
-#include "Win.h"
-#include "ForMapping.h"
 #include "Color.h"
+#include "ForMapping.h"
+#include "Win.h"
+#include <math.h>
+#include <stdio.h>
 
-void	create_clip(t_for_map *data, int height, int width, t_coordinate point)
+void create_clip(t_for_map *data, int height, int width, t_coordinate point)
 {
-	int	flag = 1;
-	int	color = 0x00ff0100;
+	int flag = 1;
+	int color = 0x00ff0100;
 	while (color != 0x00ff0000)
 	{
-		t_image_data	*img_dat = init_image_data(data->_mlx, height, width);
-		t_frame			*frame = init_frame(img_dat, point);
+		t_image_data *img_dat = init_image_data(data->_mlx, height, width);
+		t_frame *frame = init_frame(img_dat, point);
 
 		create_square((t_image_data *)(frame->_img), height, width, color);
- 		if (add_frame(data, frame))
+		if (add_frame(data, frame))
 			exit(1);
 
-		int	bit_move = ((2 - flag) < 0 ? -(2 - flag) : (2 - flag)) * 8;
+		int bit_move = ((2 - flag) < 0 ? -(2 - flag) : (2 - flag)) * 8;
 		if ((unsigned char)(color >> bit_move) == 0xff)
 		{
-			int	previous = 2 - (flag >> 1);
-			unsigned char	tmp = (color >> (previous * 8));
+			int previous = 2 - (flag >> 1);
+			unsigned char tmp = (color >> (previous * 8));
 			tmp--;
 			((unsigned char *)&color)[previous] = tmp;
 			if ((unsigned char)(color >> (previous * 8)) == 0)
@@ -29,32 +29,32 @@ void	create_clip(t_for_map *data, int height, int width, t_coordinate point)
 				if (flag == 4)
 					flag = 1;
 				else
-					flag<<=1;
+					flag <<= 1;
 			}
 		}
 		else
 		{
-			unsigned char	tmp = (color >> bit_move);
+			unsigned char tmp = (color >> bit_move);
 			tmp++;
-			int	index = (2 - flag) < 0 ? -(2 - flag) : (2 - flag);
+			int index = (2 - flag) < 0 ? -(2 - flag) : (2 - flag);
 			((unsigned char *)&color)[index] = tmp;
 		}
 	}
 }
 
-int	main(void)
+int main(void)
 {
-	void	*mlx = mlx_init();
+	void *mlx = mlx_init();
 	if (mlx == NULL)
 	{
 		printf("initialized mlx error\n");
 		exit(0);
 	}
-	void		*win = init_win(mlx, "hello world");
-	t_for_map	*data = init_data_for_mapping(mlx, win);
+	void *win = init_win(mlx, "hello world");
+	t_for_map *data = init_data_for_mapping(mlx, win);
 
-	int	height = HEIGHT;
-	int	width = WIDTH;
+	int height = HEIGHT;
+	int width = WIDTH;
 
 	/*for (size_t i = 0; i < HEIGHT / 2; i+=120)
 	{
@@ -63,29 +63,28 @@ int	main(void)
 		width -= 240;
 	}*/
 
-/*
-	for (size_t i = 0; i < WIDTH; i++)
-	{
-		t_coordinate	a={i, 0};
-		t_coordinate	b={WIDTH - i, HEIGHT};
-		t_image_data	*img = init_image_data(data->_mlx, HEIGHT, WIDTH);
-		t_frame			*frame = init_frame(img, (t_coordinate){0, 0});
+	/*
+		for (size_t i = 0; i < WIDTH; i++)
+		{
+			t_coordinate	a={i, 0};
+			t_coordinate	b={WIDTH - i, HEIGHT};
+			t_image_data	*img = init_image_data(data->_mlx, HEIGHT, WIDTH);
+			t_frame			*frame = init_frame(img, (t_coordinate){0, 0});
 
-		if (a.x == b.x)
-			continue;
-		draw_line(&a, &b, (t_image_data *)(frame->_img), 0x00ff0000);
-		add_frame(data, frame);
-	}
-*/
+			if (a.x == b.x)
+				continue;
+			draw_line(&a, &b, (t_image_data *)(frame->_img), 0x00ff0000);
+			add_frame(data, frame);
+		}
+	*/
 
+	t_coordinate a = {250, 250}; //{WIDTH, HEIGHT};
+	t_coordinate b = {0, 0};
+	t_image_data *img = init_image_data(data->_mlx, HEIGHT, WIDTH);
+	t_frame *frame = init_frame(img, (t_coordinate){0, 0});
+	// draw_line(&b, &a, (t_image_data *)(frame->_img), 0x00ff0000);
 
-	t_coordinate	a={250, 250};//{WIDTH, HEIGHT};
-	t_coordinate	b={0, 0};
-	t_image_data	*img = init_image_data(data->_mlx, HEIGHT, WIDTH);
-	t_frame			*frame = init_frame(img, (t_coordinate){0, 0});
-	//draw_line(&b, &a, (t_image_data *)(frame->_img), 0x00ff0000);
-
-	int	ret = draw_circle(&a, 100, (t_image_data *)(frame->_img), 0x00ff0000);
+	int ret = draw_circle(&a, 100, (t_image_data *)(frame->_img), 0x00ff0000);
 	if (ret)
 		printf("ret: %d\n", ret);
 	add_frame(data, frame);

02741770283b90897222786bf63bccb47cacced6
srakuma <robben2159@gmail.com>
2022-08-01 19:41:18 +0900

    norm

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -1,65 +1,58 @@
 #include "../minirt.h"
 
+static void	camera_set_base_vec(t_camera *camera)
+{
+	t_vec3d	a;
+
+	if (camera->normal.x < -0.9 || 0.9 < camera->normal.x)
+		a = (t_vec3d){0, 1, 0};
+	else
+		a = (t_vec3d){1, 0, 0};
+	camera->screen_vertical_normal = vec3d_cross(a, camera->normal);
+	camera->screen_vertical_normal = vec3d_mult(
+			camera->screen_vertical_normal,
+			1.0 / vec3d_length(camera->screen_vertical_normal));
+	camera->screen_horizon_normal = vec3d_cross(
+			camera->normal,
+			camera->screen_vertical_normal);
+}
+
+static void	camera_set_start_pos(t_camera *camera)
+{
+	const double	d = 1.0;
+	double			horizon_d;
+	double			vertical_d;
+	t_vec3d			screen_center_pos;
+	t_vec3d			screen_left_pos;
+
+	screen_center_pos = vec3d_add(camera->pos, vec3d_mult(camera->normal, d));
+	if (camera->fov == 0.0)
+	{
+		camera->screen_start_pos = screen_center_pos;
+		camera->screen_horizon_normal = (t_vec3d){0, 0, 0};
+		camera->screen_vertical_normal = (t_vec3d){0, 0, 0};
+		return ;
+	}
+	horizon_d = d * tan(camera->fov / 2.0);
+	vertical_d = horizon_d * (double)HEIGHT / (double)WIDTH;
+	camera->screen_height = 2.0 * vertical_d;
+	camera->screen_width = 2.0 * horizon_d;
+	screen_left_pos = vec3d_add(
+			screen_center_pos,
+			vec3d_mult(camera->screen_horizon_normal, -1.0 * horizon_d));
+	camera->screen_start_pos = vec3d_add(
+			screen_left_pos,
+			vec3d_mult(camera->screen_vertical_normal, -1.0 * vertical_d));
+}
+
 void	camera_set(t_camera *camera)
 {
-	{
-		// normalを単位ベクトルに変換
-		camera->normal = vec3d_mult(camera->normal, 1.0 / vec3d_length(camera->normal));
-		// fovをラジアンに変換
-		camera->fov = M_PI * camera->fov / 180.0;
-
-		// カメラからスクリーンの距離
-		double d = 1.0;
-		// スクリーン中心から端までの距離
-		double horizon_d = d * tan(camera->fov / 2.0);
-		double vertical_d = horizon_d * (double)HEIGHT / (double)WIDTH;
-
-		camera->screen_height = 2.0 * vertical_d;
-		camera->screen_width = 2.0 * horizon_d;
-		camera->dx = camera->screen_width / (double)WIDTH;
-		camera->dy = camera->screen_height / (double)HEIGHT;
-
-		// スクリーンの左右両端の点を求める--------------------------
-		// カメラからnormal方向にdだけ離れた位置の点
-		t_vec3d screen_center_pos = vec3d_add(camera->pos, vec3d_mult(camera->normal, d));
-
-		// スクリーンの基底ベクトル
-		t_vec3d	w = camera->normal; // z方向
-		t_vec3d	a;
-		if (w.x < -0.9 || 0.9 < w.x) {
-			a = (t_vec3d){0, 1, 0};
-		}
-		else {
-			a = (t_vec3d){1, 0, 0};
-		}
-		t_vec3d	v = vec3d_cross(a, w); // y方向
-		v = vec3d_mult(v, 1.0 / vec3d_length(v));
-		t_vec3d	u = vec3d_cross(w, v); // x方向
-
-		// 横幅のずらす距離
-		t_vec3d screen_left_pos = vec3d_add(
-			screen_center_pos,
-			vec3d_mult(u, -1.0 * horizon_d));
-
-		// スクリーンの左上、右上、左下の点を求める-----------------
-		t_vec3d screen_o_to_left = vec3d_sub(screen_left_pos, screen_center_pos);
-		t_vec3d camera_to_screen_o = vec3d_sub(screen_center_pos, camera->pos);
-
-		camera->screen_start_pos = vec3d_sub(screen_left_pos, vec3d_mult(v, vertical_d));
-		camera->screen_horizon_normal = u;
-		camera->screen_vertical_normal = v;
-
-		if (camera->fov == 0.0) {
-			camera->screen_start_pos = screen_center_pos;
-			camera->screen_horizon_normal = (t_vec3d){0, 0, 0};
-			camera->screen_vertical_normal = (t_vec3d){0, 0, 0};
-		}
-
-		// サンプル
-		printf("start: %f, %f, %f\n", camera->screen_start_pos.x, camera->screen_start_pos.y, camera->screen_start_pos.z);
-		printf("horizon: %f, %f, %f\n", camera->screen_horizon_normal.x, camera->screen_horizon_normal.y, camera->screen_horizon_normal.z);
-		printf("vertical: %f, %f, %f\n", camera->screen_vertical_normal.x, camera->screen_vertical_normal.y, camera->screen_vertical_normal.z);
-
-		// world->camera = camera;
-	}
+	camera->normal = vec3d_mult(
+			camera->normal,
+			1.0 / vec3d_length(camera->normal));
+	camera->fov = M_PI * camera->fov / 180.0;
+	camera_set_base_vec(camera);
+	camera_set_start_pos(camera);
+	camera->dx = camera->screen_width / (double)WIDTH;
+	camera->dy = camera->screen_height / (double)HEIGHT;
 }

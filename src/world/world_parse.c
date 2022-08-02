7abdb8e2eb56af935b32fef4cd317a8f2be9beaf
srakuma <robben2159@gmail.com>
2022-08-03 00:25:24 +0900

    parseの異常終了追加

--- src/world/world_parse.c
+++ src/world/world_parse.c
@@ -6,18 +6,20 @@
 #include <unistd.h>
 
 
-bool world_check(t_world *world) {
+bool world_check(t_world *world)
+{
 	// 存在チェック： A, C, L
 	return true;
 }
 
-bool check_strs_len(char **strs, long len){
-	long i;
+long	strs_len(char **strs)
+{
+	long	i;
 
 	i = 0;
 	while (strs[i])
 		i++;
-	return i == len;
+	return i;
 }
 
 double parse_num(char *str) {
@@ -53,215 +55,279 @@ double parse_num(char *str) {
 	return sign * (integer + decimal / digit);
 }
 
-t_vec3d parse_vec3d(char *str) {
-	char **strs = ft_split(str, ',');
-	if (!check_strs_len(strs, 3)) {
-		printf("parse_vec3d error\n");
+void	free_all(char **strs)
+{
+	long	i;
+
+	i = 0;
+	while (strs[i])
+		free(strs[i++]);
+	free(strs);
+}
+
+t_vec3d	parse_vec3d(char *str)
+{
+	char	**strs;
+	long	len;
+	t_vec3d	vec;
+
+	strs = ft_split(str, ',');
+	len = strs_len(strs);
+	if (len != 3)
+	{
+		printf("in parse vec3d, the number of elements: %ld Error\n", len);
+		free_all(strs);
 		exit(1);
 	}
-
-	t_vec3d vec;
 	vec.x = parse_num(strs[0]);
 	vec.y = parse_num(strs[1]);
 	vec.z = parse_num(strs[2]);
-	// free
-	return vec;
+	free_all(strs);
+	return (vec);
 }
 
-t_color parse_color(char *str) {
-	char **strs = ft_split(str, ',');
-	if (!check_strs_len(strs, 3)) {
-		printf("parse_color error\n");
+t_color	parse_color(char *str)
+{
+	char	**strs;
+	long	len;
+	t_color	color;
+
+	strs = ft_split(str, ',');
+	len = strs_len(strs);
+	if (len != 3)
+	{
+		printf("in parsing color, the number of elements: %ld Error\n", len);
+		free_all(strs);
 		exit(1);
 	}
-
-	t_color color;
 	color.r = parse_num(strs[0]);
 	color.g = parse_num(strs[1]);
 	color.b = parse_num(strs[2]);
-	// free
-	return color;
+	free_all(strs);
+	return (color);
 }
 
-void world_parse_ambient_light(t_world *world, char **strs, long row)
+void	world_parse_ambient_light(t_world *world, char **strs)
 {
-	if (!check_strs_len(strs, 3))
+	t_light	*light;
+	long	len;
+
+	len = strs_len(strs);
+	if (len != 3)
 	{
-		printf("column error: %ld\n", row);
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
 		exit(1);
 	}
-	t_light *light = malloc(sizeof(t_light));
+	light = malloc((t_light *)sizeof(t_light));
+	if (light == NULL)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
 	light->kind = AMBIENT_LIGHT;
-
-	long i = 1;
-	light->intensity = parse_num(strs[i++]);
-	light->color = parse_color(strs[i++]);
+	light->intensity = parse_num(strs[1]);
+	light->color = parse_color(strs[2]);
 	world_add_light(world, light);
 }
 
-void	world_parse_camera(t_world *world, char **strs, long row)
+void	world_parse_light(t_world *world, char **strs)
 {
-	if(!check_strs_len(strs, 4))
+	long	len;
+	t_light	*light;
+
+	len = strs_len(strs);
+	if (len != 3 && len != 4)
 	{
-		printf("column error: %ld\n", row);
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
 		exit(1);
 	}
-	t_camera *camera = malloc(sizeof(t_camera));
-	world->camera = camera;
-	long i = 1;
-	camera->pos = parse_vec3d(strs[i++]);
-	camera->normal = parse_vec3d(strs[i++]);
-	camera->fov = parse_num(strs[i++]);
-}
-
-void	world_parse_light(t_world *world, char **strs, long row)
-{
-	if (check_strs_len(strs, 3))
+	light = malloc(sizeof(t_light));
+	if (light == NULL)
 	{
-		t_light *light = malloc(sizeof(t_light));
-		light->kind = LIGHT;
-
-		long i = 1;
-		light->pos = parse_vec3d(strs[i++]);
-		light->intensity = parse_num(strs[i++]);
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
+	light->kind = LIGHT;
+	light->pos = parse_vec3d(strs[1]);
+	light->intensity = parse_num(strs[2]);
+	if (len == 3)
 		light->color = (t_color){255.0, 255.0, 255.0};
-		world_add_light(world, light);
-	}
-	else if (check_strs_len(strs, 4))
-	{
-		t_light *light = malloc(sizeof(t_light));
-		light->kind = LIGHT;
-
-		long i = 1;
-		light->pos = parse_vec3d(strs[i++]);
-		light->intensity = parse_num(strs[i++]);
-		light->color = parse_color(strs[i++]);
-		world_add_light(world, light);
-	}
-	else {
-		printf("column error: %ld\n", row);
-		exit(1);
-	}
+	world_add_light(world, light);
 }
 
-void world_parse_sphere(t_world *world, char **strs, long row) {
-	if(!check_strs_len(strs, 4)) {
-		printf("column error: %ld\n", row);
+void	world_parse_camera(t_world *world, char **strs)
+{
+	long		len;
+	t_camera	*camera;
+
+	len = strs_len(strs);
+	if (len != 4)
+	{
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
 		exit(1);
 	}
-	t_shape *shape = malloc(sizeof(t_shape));
+	camera = malloc((t_camera *)sizeof(t_camera));
+	if (camera == NULL)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
+	world->camera = camera;
+	camera->pos = parse_vec3d(strs[1]);
+	camera->normal = parse_vec3d(strs[2]);
+	camera->fov = parse_num(strs[3]);
+}
+
+void	world_parse_sphere(t_world *world, char **strs)
+{
+	long	len;
+	t_shape	*shape;
+
+	len = strs_len(strs);
+	if (len != 4)
+	{
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
+		exit(1);
+	}
+	shape = (t_shape *)malloc(sizeof(t_shape));
+	if (shape == NULL)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
 	shape->kind = SPHERE;
-
-	long i = 1;
-	shape->center =	parse_vec3d(strs[i++]);
-	shape->radius =	parse_num(strs[i++]) / 2.0;
-	shape->color = parse_color(strs[i++]);
-
-	shape->ka = color_mult_num(shape->color, 1.0 / 255.0); // 環境光反射係数
-    shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);   // 拡散反射係数
-    // shape->kd = (t_color){0.0, 0.69, 0.0};   // 拡散反射係数
-    shape->ks = (t_color){0.3, 0.3, 0.3};    // 鏡面反射係数
-	shape->shininess = 16.0;                  //光沢度
+	shape->center =	parse_vec3d(strs[1]);
+	shape->radius =	parse_num(strs[2]) / 2.0;
+	shape->color = parse_color(strs[3]);
+	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
+	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
+	shape->ks = (t_color){0.3, 0.3, 0.3};
+	shape->shininess = 16.0;
 	world_add_shape(world, shape);
 }
 
-void world_parse_plane(t_world *world, char **strs, long row) {
-	if(!check_strs_len(strs, 4) && (!check_strs_len(strs, 5) && ft_strcmp(strs[4], "mirror") == 0)) {
-		printf("column error: %ld\n", row);
+void	world_parse_plane(t_world *world, char **strs)
+{
+	long	len;
+	t_shape	*shape;
+
+	len = strs_len(strs);
+	if (len != 4 && (len != 5 || ft_strcmp(strs[4], "mirror")))
+	{
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
 		exit(1);
 	}
-	t_shape *shape = malloc(sizeof(t_shape));
-	shape->kind =PLANE;
-
-	long i = 1;
-	shape->point = parse_vec3d(strs[i++]);
-	shape->oriental_normal = parse_vec3d(strs[i++]);
-	shape->color = parse_color(strs[i++]);
-
-	shape->ka = color_mult_num(shape->color, 1.0 / 255.0); // 環境光反射係数
-    shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);   // 拡散反射係数
-    // shape->kd = (t_color){0.0, 0.69, 0.0};   // 拡散反射係数
-    shape->ks = (t_color){0.3, 0.3, 0.3};    // 鏡面反射係数
-	shape->shininess = 16.0;                  //光沢度
+	t_shape *shape = (t_shape *)malloc(sizeof(t_shape));
+	if (shape == NULL)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
+	shape->kind = PLANE;
+	shape->point = parse_vec3d(strs[1]);
+	shape->oriental_normal = parse_vec3d(strs[2]);
+	shape->color = parse_color(strs[3]);
+	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
+	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
+	shape->ks = (t_color){0.3, 0.3, 0.3};
+	shape->shininess = 16.0;
 	if (check_strs_len(strs, 5))
 		shape->is_mirror = true;
 	world_add_shape(world, shape);
 }
 
-void world_parse_cylinder(t_world *world, char **strs, long row) {
-    if(!check_strs_len(strs, 6)) {
-		printf("column error: %ld\n", row);
+void world_parse_cylinder(t_world *world, char **strs)
+{
+	long	len;
+	t_shape	*shape;
+
+	len = strs_len(strs);
+	if(len != 6)
+	{
+		printf("in %s, the number of elements: %ld Error\n", __func__, len);
+		free_all(strs);
 		exit(1);
 	}
-	t_shape *shape = malloc(sizeof(t_shape));
+	t_shape *shape = (t_shape *)malloc(sizeof(t_shape));
+	if (shape == NULL)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		free_all(strs);
+		exit(1);
+	}
 	shape->kind = CYLINDER;
-
-	long i = 1;
-	shape->center =	parse_vec3d(strs[i++]);
-	shape->oriental_normal = parse_vec3d(strs[i++]);
-	shape->radius =	parse_num(strs[i++]) / 2.0;
-	shape->height =	parse_num(strs[i++]);
-	shape->color = parse_color(strs[i++]);
-
-	shape->ka = color_mult_num(shape->color, 1.0 / 255.0); // 環境光反射係数
-    shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);   // 拡散反射係数
-    // shape->kd = (t_color){0.0, 0.69, 0.0};   // 拡散反射係数
-    shape->ks = (t_color){0.3, 0.3, 0.3};    // 鏡面反射係数
-	shape->shininess = 16.0;                  //光沢度
+	shape->center =	parse_vec3d(strs[1]);
+	shape->oriental_normal = parse_vec3d(strs[2]);
+	shape->radius =	parse_num(strs[3]) / 2.0;
+	shape->height =	parse_num(strs[4]);
+	shape->color = parse_color(strs[5]);
+	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
+	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
+	shape->ks = (t_color){0.3, 0.3, 0.3};
+	shape->shininess = 16.0;
 	world_add_shape(world, shape);
 }
 
-void world_parse(t_world *world, char *filename) {
-	int fd = open(filename, O_RDONLY);
-	if (fd <= 0) {
-		printf("cant open file\n");
+void	world_parse(t_world *world, char *filename)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("cannot open file\n");
 		exit(1);
 	}
-
-	long row = -1;
-	while (true) {
-		char *line = get_next_line(fd);
+	while (true)
+	{
+		line = get_next_line(fd);
 		if(line == NULL)
 			break;
-		row++;
-
 		if(line[0] == '\0')
 		{
 			free(line);
 			continue;
 		}
-
-		//スペース区切り
 		char **strs = ft_split(line, ' ');
+		if (strs == NULL)
+		{
+			printf("in %s, failed to malloc\n", __func__);
+			exit(1);
+		}
 
-		// Ambient Light
 		if (ft_strcmp(strs[0], "A") == 0)
-			world_parse_ambient_light(world, strs, row);
-		// Camera
+			world_parse_ambient_light(world, strs);
 		else if (ft_strcmp(strs[0], "C") == 0)
-			world_parse_camera(world, strs, row);
-		// Light
+			world_parse_camera(world, strs);
 		else if (ft_strcmp(strs[0], "L") == 0)
-			world_parse_light(world, strs, row);
-		// Sphere
+			world_parse_light(world, strs);
 		else if (ft_strcmp(strs[0], "sp") == 0)
-            world_parse_sphere(world, strs, row);
-		// Plane
+			world_parse_sphere(world, strs);
 		else if (ft_strcmp(strs[0], "pl") == 0)
-            world_parse_plane(world, strs, row);
-		// Cylinder
+			world_parse_plane(world, strs);
 		else if (ft_strcmp(strs[0], "cy") == 0)
-            world_parse_cylinder(world, strs, row);
-		else {
+			world_parse_cylinder(world, strs);
+		else
+		{
 			printf("not exist keyword: %s", strs[0]);
+			free(line);
 			exit(1);
 		}
 		free(line);
 	}
-
 	if (!world_check(world)) {
 		printf("parse error\n");
 		exit(1);
 	}
 }
-

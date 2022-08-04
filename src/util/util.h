af103cca36d9de7eeff53398a82e684ccf22731b
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-04 10:49:14 +0900

    typo

--- src/util/util.h
+++ src/util/util.h
@@ -6,14 +6,14 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 10:42:13 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/08/04 10:42:23 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/08/04 10:49:04 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_H
 # define UTIL_H
 
-double	zxdouble_max(double a, double b);
+double	double_max(double a, double b);
 double	double_min(double a, double b);
 
 #endif

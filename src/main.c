ea115620faa0daa95c375164faed31429d1c67b4
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-04 10:32:56 +0900

    norm

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "minirt.h"
 
-int key_event(int key_code, void *param) {
+int key_event(int key_code, void *param)
+{
     printf("key_code: %d\n", key_code);
     if (key_code == 65307)
         exit(0);
     return (0);
 }
 
-t_shape *compute_intersected_shape(t_world *world, t_vec3d to_screen) {
+t_shape *compute_intersected_shape(t_world *world, t_vec3d to_screen)
+{
     t_vec3d camera_to_screen;
     t_shape *now_shape;
     t_shape *intersected_shape;
@@ -18,10 +20,12 @@ t_shape *compute_intersected_shape(t_world *world, t_vec3d to_screen) {
     intersected_shape = NULL;
     minimum_t = -1;
     now_shape = world->shape_list;
-    while (now_shape) {
+    while (now_shape)
+    {
         t = shape_get_intersection(camera_to_screen,
                                    vec3d_camera(world->camera), now_shape);
-        if (t >= 1.0 && (intersected_shape == NULL || minimum_t > t)) {
+        if (t >= 1.0 && (intersected_shape == NULL || minimum_t > t))
+        {
             intersected_shape = now_shape;
             minimum_t = t;
         }
@@ -30,14 +34,16 @@ t_shape *compute_intersected_shape(t_world *world, t_vec3d to_screen) {
     return intersected_shape;
 }
 
-t_color compute_ambient_light(t_world *world, t_shape *shape, t_light *light) {
+t_color compute_ambient_light(t_world *world, t_shape *shape, t_light *light)
+{
     t_color ia = color_mult_num(light->color, light->intensity / 255.0);
     t_color radience_amb = color_mult_color(ia, shape->ka);
     return radience_amb;
 }
 
 t_vec3d compute_intersected_pos(t_camera *camera, t_vec3d to_screen,
-                                t_shape *shape) {
+                                t_shape *shape)
+{
     t_vec3d o_to_screen = vec3d_camera_to_screen(camera, to_screen);
     double t = shape_get_intersection(o_to_screen, camera->pos, shape);
     t_vec3d pos = vec3d_add(camera->pos, vec3d_mult(o_to_screen, t));
@@ -45,10 +51,12 @@ t_vec3d compute_intersected_pos(t_camera *camera, t_vec3d to_screen,
 }
 
 bool compute_is_shadow(t_world *world, t_shape *shape, t_light *light,
-                       t_vec3d intersected_pos) {
+                       t_vec3d intersected_pos)
+{
     t_shape *now_shape = world->shape_list;
     t_vec3d int_to_light_dir = vec3d_sub(light->pos, intersected_pos);
-    while (now_shape) {
+    while (now_shape)
+    {
         double t = shape_get_intersection(
             int_to_light_dir,
             vec3d_add(intersected_pos,
@@ -71,7 +79,8 @@ bool compute_is_shadow(t_world *world, t_shape *shape, t_light *light,
 // }
 
 t_color compute_light(t_world *world, t_vec3d to_screen, t_shape *shape,
-                      t_light *light) {
+                      t_light *light)
+{
     t_color intensity = (t_color){0.0, 0.0, 0.0};
 
     // 交差位置: 球面上の点 P = O + tD
@@ -79,7 +88,8 @@ t_color compute_light(t_world *world, t_vec3d to_screen, t_shape *shape,
         compute_intersected_pos(world->camera, to_screen, shape);
     t_vec3d o_to_screen = vec3d_camera_to_screen(world->camera, to_screen);
     // 付影処理
-    if (compute_is_shadow(world, shape, light, intersected_pos)) {
+    if (compute_is_shadow(world, shape, light, intersected_pos))
+    {
         light = light->next;
         return (t_color){0.0, 0.0, 0.0};
     }
@@ -89,7 +99,8 @@ t_color compute_light(t_world *world, t_vec3d to_screen, t_shape *shape,
     // 視線ベクトルの逆単位ベクトルと法線ベクトルのなす角が90度以上なら逆に向ける
     // 視線ベクトルの逆単位ベクトル
     t_vec3d v = vec3d_unit(vec3d_mult(o_to_screen, -1.0));
-    if (vec3d_dot(v, normal) <= 0.0) {
+    if (vec3d_dot(v, normal) <= 0.0)
+    {
         normal = vec3d_mult(normal, -1.0);
     }
     t_color ii = color_mult_num(light->color, light->intensity / 255.0);
@@ -103,7 +114,8 @@ t_color compute_light(t_world *world, t_vec3d to_screen, t_shape *shape,
     intensity = color_add_color(intensity, radience_dif);
     // 鏡面反射光 ----------------------------------------
     t_color radience_spe = (t_color){0.0, 0.0, 0.0};
-    if (cosA > 0) {
+    if (cosA > 0)
+    {
         //正反射ベクトル
         t_vec3d r = vec3d_sub(vec3d_mult(normal, 2 * (cosA)), pos_to_light_dir);
         radience_spe = color_mult_num(
@@ -114,14 +126,16 @@ t_color compute_light(t_world *world, t_vec3d to_screen, t_shape *shape,
     return intensity;
 }
 
-t_color compute_brightness(t_world *world, t_vec3d to_screen, t_shape *shape) {
+t_color compute_brightness(t_world *world, t_vec3d to_screen, t_shape *shape)
+{
     t_color intensity = (t_color){0.0, 0.0, 0.0};
     t_color add_intensity;
 
     if (shape == NULL)
         return (t_color){0.0, 0.0, 0.0};
     t_light *now_light = world->light_list;
-    while (now_light) {
+    while (now_light)
+    {
         // 環境光
         if (now_light->kind == AMBIENT_LIGHT)
             add_intensity = compute_ambient_light(world, shape, now_light);
@@ -134,7 +148,8 @@ t_color compute_brightness(t_world *world, t_vec3d to_screen, t_shape *shape) {
     return intensity;
 }
 
-t_color compute_color_of_pixel(t_world *world, t_vec3d to_screen) {
+t_color compute_color_of_pixel(t_world *world, t_vec3d to_screen)
+{
     t_vec3d o_to_screen = vec3d_camera_to_screen(world->camera, to_screen);
     t_shape *intersected_shape = compute_intersected_shape(world, to_screen);
     double minimum_t = shape_get_intersection(
@@ -156,9 +171,11 @@ t_color compute_color_of_pixel(t_world *world, t_vec3d to_screen) {
     return screen_color;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     void *mlx = mlx_init();
-    if (mlx == NULL) {
+    if (mlx == NULL)
+    {
         printf("initialized mlx error\n");
         exit(0);
     }
@@ -172,12 +189,14 @@ int main(int argc, char *argv[]) {
     long y;
     long x;
     y = 0;
-    while (y < HEIGHT) {
+    while (y < HEIGHT)
+    {
         t_vec3d screen_p_yaxis = vec3d_add(
             camera->screen_start_pos,
             vec3d_mult(camera->screen_vertical_normal, camera->dy * (double)y));
         x = 0;
-        while (x < WIDTH) {
+        while (x < WIDTH)
+        {
             t_vec3d to_screen = vec3d_add(
                 screen_p_yaxis, vec3d_mult(camera->screen_horizon_normal,
                                            camera->dx * (double)x));

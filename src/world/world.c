65254d300786cfc540d78abf4af9c6de509ff403
srakuma <robben2159@gmail.com>
2022-08-05 21:36:34 +0900

    エラー対処

--- src/world/world.c
+++ src/world/world.c
@@ -1,13 +1,34 @@
 #include "../minirt.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 // ワールド情報は現状直接書いてる
 // TODO: パースした情報を入れる
-t_world	*world_init(char *filename) {
-	const t_color bg =(t_color){30.0, 30.0, 30.0};
-	t_world *world;
+t_world	*world_init(char *filename)
+{
+	const t_color	bg =(t_color){30.0, 30.0, 30.0};
+	t_world			*world;
+	char			*line;
+	int				fd;
 
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("cannot open file");
+		//printf("cannot open file\n");
+		exit(1);
+	}
 	world = ft_calloc(1, sizeof(t_world));
+	if (!world)
+	{
+		printf("in %s, failed to malloc\n", __func__);
+		close(fd);
+		exit(1);
+	}
 	world->bg = bg;
-	world_parse(world, filename);
-	return world;
+	world_parse(world, filename, fd);
+	close(fd);
+	return (world);
 }
--- src/world/world_parse.c
+++ src/world/world_parse.c
@@ -1,15 +1,28 @@
 #include "../minirt.h"
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-
-
-bool world_check(t_world *world)
+bool	world_check(t_world *world)
 {
-	// 存在チェック： A, C, L
-	return true;
+	t_light	*light;
+	bool	a;
+	bool	l;
+
+	light = world->light_list;
+	a = false;
+	l = false;
+	while (light)
+	{
+		if (light->kind == AMBIENT_LIGHT)
+		{
+			if (a == false)
+				a = true;
+			else
+				return (false);
+		}
+		else
+			l = true;
+		light = light->next;
+	}
+	return (a && l && (world->camera));
 }
 
 long	strs_len(char **strs)
@@ -19,7 +32,7 @@ long	strs_len(char **strs)
 	i = 0;
 	while (strs[i])
 		i++;
-	return i;
+	return (i);
 }
 
 void	*ft_xalloc(size_t count, size_t size, const char *func_name)
@@ -46,7 +59,7 @@ void	*ft_xalloc(size_t count, size_t size, const char *func_name)
 	return (ptr);
 }
 
-double parse_num(char *str)
+double	parse_num(char *str)
 {
 	double	integer;
 	double	sign;
@@ -86,14 +99,17 @@ void	free_all(char **strs)
 	free(strs);
 }
 
-void	number_of_element_error(char **strs, const char *func_name, long len)
+void	number_of_element_error(
+	char **strs,
+	const char *func_name,
+	long len)
 {
 	printf("in %s, the number of elements: %ld Error\n", func_name, len);
 	free_all(strs);
 	exit(1);
 }
 
-bool	check_in_range(double val[], size_t len,  double max, double min)
+bool	check_in_range(double val[], size_t len, double max, double min)
 {
 	size_t	i;
 
@@ -107,12 +123,58 @@ bool	check_in_range(double val[], size_t len,  double max, double min)
 	return (true);
 }
 
+void	world_destuctor(t_world *world)
+{
+	t_light	*light;
+	t_shape	*shape;
+
+	light = world->light_list;
+	while (light)
+	{
+		world->light_list = light->next;
+		free(light);
+		light = world->light_list;
+	}
+	shape = world->shape_list;
+	while (shape)
+	{
+		world->shape_list = shape->next;
+		free(shape);
+		shape = world->shape_list;
+	}
+	free(world->camera);
+	free(world);
+}
+
+bool	check_num_of_elements(char *str)
+{
+	char	*commma;
+	long	cnt;
+
+	cnt = 0;
+	commma = str;
+	while (true)
+	{
+		commma = ft_strchr(commma, ',');
+		if (commma == NULL)
+			break ;
+		commma++;
+		cnt++;
+	}
+	return (cnt == 2);
+}
+
 t_vec3d	parse_vec3d(char *str)
 {
 	char	**strs;
 	long	len;
 	t_vec3d	vec;
 
+	if (!check_num_of_elements(str))
+	{
+		printf("Wrong number of elements\n");
+		exit(1);
+	}
 	strs = ft_split(str, ',');
 	len = strs_len(strs);
 	if (len != 3)
@@ -120,7 +182,6 @@ t_vec3d	parse_vec3d(char *str)
 	vec.x = parse_num(strs[0]);
 	vec.y = parse_num(strs[1]);
 	vec.z = parse_num(strs[2]);
-	free_all(strs);
 	return (vec);
 }
 
@@ -130,6 +191,11 @@ t_color	parse_color(char *str)
 	long	len;
 	t_color	color;
 
+	if (!check_num_of_elements(str))
+	{
+		printf("Wrong number of elements\n");
+		exit(1);
+	}
 	strs = ft_split(str, ',');
 	len = strs_len(strs);
 	if (len != 3)
@@ -137,8 +203,7 @@ t_color	parse_color(char *str)
 	color.r = parse_num(strs[0]);
 	color.g = parse_num(strs[1]);
 	color.b = parse_num(strs[2]);
-	free_all(strs);
-	if (!check_in_range((double[]){color.r, color.g, color.b}, 3, 255.0, 0.0))
+	if (!check_in_range((double []){color.r, color.g, color.b}, 3, 255.0, 0.0))
 	{
 		printf("color is out of range\n");
 		exit(1);
@@ -148,8 +213,8 @@ t_color	parse_color(char *str)
 
 void	world_parse_ambient_light(t_world *world, char **strs)
 {
-	t_light	*light;
 	long	len;
+	t_light	*light;
 
 	len = strs_len(strs);
 	if (len != 3)
@@ -196,6 +261,11 @@ void	world_parse_camera(t_world *world, char **strs)
 	double		*tmp;
 	t_camera	*camera;
 
+	if (world->camera)
+	{
+		printf("Two or more C's exist\n");
+		exit(1);
+	}
 	len = strs_len(strs);
 	if (len != 4)
 		number_of_element_error(strs, __func__, len);
@@ -203,7 +273,7 @@ void	world_parse_camera(t_world *world, char **strs)
 	world->camera = camera;
 	camera->pos = parse_vec3d(strs[1]);
 	camera->normal = parse_vec3d(strs[2]);
-	tmp = (double[]){camera->normal.x, camera->normal.y, camera->normal.z};
+	tmp = (double []){camera->normal.x, camera->normal.y, camera->normal.z};
 	if (!check_in_range(tmp, 3, 1.0, -1.0))
 	{
 		printf("camera's normal is out of range\n");
@@ -227,8 +297,8 @@ void	world_parse_sphere(t_world *world, char **strs)
 		number_of_element_error(strs, __func__, len);
 	shape = (t_shape *)ft_xalloc(sizeof(t_shape), 1, __func__);
 	shape->kind = SPHERE;
-	shape->center =	parse_vec3d(strs[1]);
-	shape->radius =	parse_num(strs[2]) / 2.0;
+	shape->center = parse_vec3d(strs[1]);
+	shape->radius = parse_num(strs[2]) / 2.0;
 	shape->color = parse_color(strs[3]);
 	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
 	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
@@ -250,7 +320,7 @@ void	world_parse_plane(t_world *world, char **strs)
 	shape->kind = PLANE;
 	shape->point = parse_vec3d(strs[1]);
 	shape->normal = parse_vec3d(strs[2]);
-	tmp = (double[]){shape->normal.x, shape->normal.y, shape->normal.z};
+	tmp = (double []){shape->normal.x, shape->normal.y, shape->normal.z};
 	if (!check_in_range(tmp, 3, 1.0, -1.0))
 	{
 		printf("sphere's normal is out of range\n");
@@ -266,27 +336,27 @@ void	world_parse_plane(t_world *world, char **strs)
 	world_add_shape(world, shape);
 }
 
-void world_parse_cylinder(t_world *world, char **strs)
+void	world_parse_cylinder(t_world *world, char **strs)
 {
 	long	len;
 	double	*tmp;
 	t_shape	*shape;
 
 	len = strs_len(strs);
-	if(len != 6)
+	if (len != 6)
 		number_of_element_error(strs, __func__, len);
 	shape = (t_shape *)ft_xalloc(sizeof(t_shape), 1, __func__);
 	shape->kind = CYLINDER;
-	shape->center =	parse_vec3d(strs[1]);
+	shape->center = parse_vec3d(strs[1]);
 	shape->normal = parse_vec3d(strs[2]);
-	tmp = (double[]){shape->normal.x, shape->normal.y, shape->normal.z};
+	tmp = (double []){shape->normal.x, shape->normal.y, shape->normal.z};
 	if (!check_in_range(tmp, 3, 1.0, -1.0))
 	{
 		printf("cylinder's normal is out of range\n");
 		exit(1);
 	}
-	shape->radius =	parse_num(strs[3]) / 2.0;
-	shape->height =	parse_num(strs[4]);
+	shape->radius = parse_num(strs[3]) / 2.0;
+	shape->height = parse_num(strs[4]);
 	shape->color = parse_color(strs[5]);
 	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
 	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
@@ -295,54 +365,55 @@ void world_parse_cylinder(t_world *world, char **strs)
 	world_add_shape(world, shape);
 }
 
-void	world_parse(t_world *world, char *filename)
+void	world_parse_line(t_world *world, char **strs)
 {
-	int		fd;
-	char	*line;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (ft_strcmp(strs[0], "A") == 0)
+		world_parse_ambient_light(world, strs);
+	else if (ft_strcmp(strs[0], "C") == 0)
+		world_parse_camera(world, strs);
+	else if (ft_strcmp(strs[0], "L") == 0)
+		world_parse_light(world, strs);
+	else if (ft_strcmp(strs[0], "sp") == 0)
+		world_parse_sphere(world, strs);
+	else if (ft_strcmp(strs[0], "pl") == 0)
+		world_parse_plane(world, strs);
+	else if (ft_strcmp(strs[0], "cy") == 0)
+		world_parse_cylinder(world, strs);
+	else
 	{
-		printf("cannot open file\n");
+		printf("not exist keyword: %s\n", strs[0]);
 		exit(1);
 	}
+}
+
+void	world_parse(t_world *world, char *filename, int fd)
+{
+	char	*line;
+	char	**strs;
+	int		*count;
+
+	count = (int []){0, 0};
 	while (true)
 	{
 		line = get_next_line(fd);
-		if(line == NULL)
-			break;
-		if(line[0] == '\0')
+		if (line == NULL)
+			break ;
+		else if (line[0] != '\0')
 		{
-			free(line);
-			continue;
+			strs = ft_split(line, ' ');
+			if (strs == NULL)
+			{
+				printf("in %s, failed to malloc\n", __func__);
+				free(line);
+				exit(1);
+			}
+			world_parse_line(world, strs);
+			free_all(strs);
 		}
-		char **strs = ft_split(line, ' ');
 		free(line);
-		if (strs == NULL)
-		{
-			printf("in %s, failed to malloc\n", __func__);
-			exit(1);
-		}
-		if (ft_strcmp(strs[0], "A") == 0)
-			world_parse_ambient_light(world, strs);
-		else if (ft_strcmp(strs[0], "C") == 0)
-			world_parse_camera(world, strs);
-		else if (ft_strcmp(strs[0], "L") == 0)
-			world_parse_light(world, strs);
-		else if (ft_strcmp(strs[0], "sp") == 0)
-			world_parse_sphere(world, strs);
-		else if (ft_strcmp(strs[0], "pl") == 0)
-			world_parse_plane(world, strs);
-		else if (ft_strcmp(strs[0], "cy") == 0)
-			world_parse_cylinder(world, strs);
-		else
-		{
-			printf("not exist keyword: %s", strs[0]);
-			exit(1);
-		}
-		free_all(strs);
 	}
-	if (!world_check(world)) {
+	if (!world_check(world))
+	{
 		printf("parse error\n");
 		exit(1);
 	}

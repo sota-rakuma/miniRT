1743b9e2f2f49ced4e7777c9bc31094f1bcc7d3f
srakuma <robben2159@gmail.com>
2022-08-06 23:51:13 +0900

    norm

--- src/img.c
+++ src/img.c
@@ -1,32 +1,37 @@
 #include "minirt.h"
 
-t_img* img_init(void* mlx, int height, int width) {
-	t_img* new;
+t_img	*img_init(void *mlx, int height, int width)
+{
+	t_img	*new;
 
-	new = (t_img*)malloc(sizeof(t_img));
-	if (new == NULL) {
+	new = (t_img *)malloc(sizeof(t_img));
+	if (new == NULL)
+	{
 		printf("failed to initialize image data\n");
 		exit(1);
 	}
 	new->_img = mlx_new_image(mlx, width, height);
-	if (new->_img == NULL) {
+	if (new->_img == NULL)
+	{
 		printf("faild to make new image\n");
 		exit(1);
 	}
 	new->_addr = mlx_get_data_addr(new->_img, &(new->_bits_per_pixel),
-	                               &(new->_line_length), &(new->_endian));
+			&(new->_line_length), &(new->_endian));
 	return (new);
 }
 
-void img_pixel_put(t_img* data, int x, int y, int color) {
-	char* dst;
+void	img_pixel_put(t_img *data, int x, int y, int color)
+{
+	char	*dst;
 
-	dst = data->_addr +
-	      (y * data->_line_length + x * (data->_bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	dst = data->_addr \
+		+ (y * data->_line_length + x * (data->_bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
-void img_destruct(void* mlx, t_img* data) {
+void	img_destruct(void *mlx, t_img *data)
+{
 	mlx_destroy_image(mlx, data->_img);
 	free(data);
 }
--- src/img.h
+++ src/img.h
@@ -1,16 +1,17 @@
 #ifndef IMG_H
-#define IMG_H
+# define IMG_H
 
-typedef struct s_img {
-	void* _img;
-	char* _addr;
-	int _bits_per_pixel;
-	int _line_length;
-	int _endian;
-} t_img;
+typedef struct s_img
+{
+	void	*_img;
+	char	*_addr;
+	int		_bits_per_pixel;
+	int		_line_length;
+	int		_endian;
+}				t_img;
 
-t_img* img_init(void* mlx, int height, int width);
-void img_pixel_put(t_img* data, int x, int y, int color);
-void img_destruct(void* mlx, t_img* data);
+t_img	*img_init(void *mlx, int height, int width);
+void	img_pixel_put(t_img *data, int x, int y, int color);
+void	img_destruct(void *mlx, t_img *data);
 
 #endif /* IMG_H */

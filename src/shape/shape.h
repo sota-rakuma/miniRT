eaef5b7712863931f1cbaef9b4f75f1bc1e17d9b
srakuma <robben2159@gmail.com>
2022-08-07 00:00:12 +0900

    norm

--- src/shape/shape.h
+++ src/shape/shape.h
@@ -6,7 +6,7 @@
 /*   By: srakuma <srakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 10:44:17 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/08/06 21:19:50 by srakuma          ###   ########.fr       */
+/*   Updated: 2022/08/06 23:59:47 by srakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "../color/color.h"
 # include "../vec3d/vec3d.h"
-#include "../util/util.h"
+# include "../util/util.h"
 # include <stdbool.h>
 
 // shape
@@ -23,34 +23,23 @@ typedef enum e_shape_kind {
 	SPHERE,
 	PLANE,
 	CYLINDER,
-} t_shape_kind;
+}	t_shape_kind;
 
 typedef struct s_shape {
 	struct s_shape	*next;
 	t_shape_kind	kind;
-
-	// common
 	t_color			color;
 	t_color			ka;
 	t_color			kd;
 	t_color			ks;
 	double			shininess;
-
-	// sphere
 	t_vec3d			center;
 	double			radius;
-
-	// plane
 	t_vec3d			normal;
 	t_vec3d			point;
 	bool			is_mirror;
-
-	// cylinder
-	// t_vec3d			center;
-	// t_vec3d			normal;
-	// double			radius;
-	double				height;
-} t_shape;
+	double			height;
+}	t_shape;
 
 double	with_sphere(t_vec3d o_to_screen, t_vec3d o, t_shape *shape);
 double	with_plane(t_vec3d o_to_screen, t_vec3d o, t_shape *shape);

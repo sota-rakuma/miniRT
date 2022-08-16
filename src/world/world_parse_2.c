2a13b40ef58aac708ab1d83f08dd2e4243db832c
srakuma <robben2159@gmail.com>
2022-08-16 13:04:27 +0900

    直径が0又は負の数の時のエラー処理

--- src/world/world_parse_2.c
+++ src/world/world_parse_2.c
@@ -1,5 +1,15 @@
 #include "../minirt.h"
 
+double	parse_diameter(char *str, long row)
+{
+	double	diameter;
+
+	diameter = parse_num(str, row);
+	if (diameter <= 0)
+		error_line_msg(row, "diameter is less than or equal to 0");
+	return (diameter / 2.0);
+}
+
 void	world_parse_sphere(t_world *world, char **strs, long row)
 {
 	const long	len = strs_len(strs);
@@ -10,7 +20,8 @@ void	world_parse_sphere(t_world *world, char **strs, long row)
 	shape = (t_shape *)ft_xalloc(sizeof(t_shape), 1, __func__);
 	shape->kind = SPHERE;
 	shape->center = parse_vec3d(strs[1], row);
-	shape->radius = parse_num(strs[2], row) / 2.0;
+	shape->radius = parse_diameter(strs[2], row);
+	printf("shape->radius: %.10f\n", shape->radius);
 	shape->color = parse_color(strs[3], row);
 	shape->ka = color_mult_num(shape->color, 1.0 / 255.0);
 	shape->kd = color_mult_num(shape->color, 1.0 / 255.0 * 0.69);
@@ -58,7 +69,7 @@ void	world_parse_cylinder(t_world *world, char **strs, long row)
 	if (!check_in_range(tmp, 3, 1.0, -1.0) || vec3d_length(shape->normal) == 0)
 		error_line_msg(row, "normal has wrong vector");
 	shape->normal = vec3d_unit(shape->normal);
-	shape->radius = parse_num(strs[3], row) / 2.0;
+	shape->radius = parse_diameter(strs[3], row);
 	shape->height = parse_num(strs[4], row);
 	shape->color = parse_color(strs[5], row);
 	shape->center = parse_vec3d(strs[1], row);
